6169026a4a4c6b415955a30f611795475cdcc81b
Annie Huang <annie.hn@gmail.com>
2020-04-06 16:52:21 +1000

    Add the version and key command

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+const { program } = require('commander');
+const pkg = require('../package');
+
+program
+    .version(pkg.version)
+    .command('key', 'Manage API key -- https://nomics.com')
+    .parse(process.argv);
 
 console.log('Hello from conindex');
+
+/*
+console.log(process.argv);
+C:\react\coindex-cli>coindex key
+[
+    'C:\\Program Files\\nodejs\\node.exe',
+    'C:\\Users\\Annie\\AppData\\Roaming\\npm\\node_modules\\coindex\\bin\\coindex.js',
+    'key'
+]
+console.log(process.argv[2]);
+C:\react\coindex-cli>coindex key
+key
+*/
